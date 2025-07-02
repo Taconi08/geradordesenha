7c2d8ce4d34e484dc67683a48596c653f2227b20
Taconi08 <159157513+Taconi08@users.noreply.github.com>
2025-07-02 12:30:44 -0300

    2

--- main.js
+++ main.js
@@ -57,9 +57,9 @@ if(checkbox[3].checked){
 
     let senha = '';
     for( let i = 0; i < tamanhoSenha; i++){
-        let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+        let numeroAleatorio = Math.random()*alfabeto.length;
         numeroAleatorio = Math.floor(numeroAleatorio);
-        senha = senha + letrasMaiusculas[numeroAleatorio];
+        senha = senha + alfabeto[numeroAleatorio];
     }
     campoSenha.value = senha;
     classificaSenha();
